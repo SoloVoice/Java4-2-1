3806ba3ae5c9129a24b944fa039b269611c81493
SoloVoice <a.v.rustamov@gmail.com>
2021-02-23 18:55:34 +0300

    homework

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,48 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Client {
+
+    private SocketChannel channel;
+    private Selector selector;
+    private ByteBuffer buffer;
+
+    public Client() throws IOException {
+        channel = SocketChannel.open();
+        channel.configureBlocking(false);
+        selector = Selector.open();
+        channel.register(selector, SelectionKey.OP_CONNECT);
+        channel.connect(new InetSocketAddress("localhost", 8189));
+        BlockingQueue<String> queue = new ArrayBlockingQueue<>(2);
+        new Thread(() -> {
+            Scanner scanner = new Scanner(System.in);
+            while (true) {
+                String line = scanner.nextLine();
+                if (line.equals("/q")) {
+                    System.exit(0);
+                }
+                try {
+                    queue.put(line);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                SelectionKey key = channel.keyFor(selector);
+                key.interestOps(SelectionKey.OP_WRITE);
+                selector.wakeup();
+            }
+        }).start();
+
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Client();
+    }
 }
